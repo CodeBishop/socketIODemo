07be747bd5e300af87dfd4c8e53f40258b6e2b67
sbishop <sbishop@navarik.com>
2019-02-15 15:13:14 -0800

    Add trivial client and make server listen for it.

--- basics.js
+++ basics.js
@@ -5,4 +5,15 @@ const httpServer = http.createServer((req, res) => {
   res.end("Browse localhost:9000 to see this string")
 })
 
+// Setup socketio to listen to httpServer listening to http.
+const io = socketio(httpServer)
+
+io.on('connection', (socket, req) => {
+  socket.emit('my custom event type for welcoming', 'Run the server then open basics.html. F12->Console and this string should be in the Message.')
+
+  socket.on('my other custom event type', (msg) => {
+    console.log(msg);
+  })
+})
+
 httpServer.listen(9000)
