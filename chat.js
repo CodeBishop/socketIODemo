2d1c325e5aeb2661d000b3c6fa21a56c0847190b
sbishop <sbishop@navarik.com>
2019-02-19 08:56:28 -0800

    Add some comments.

--- chat.js
+++ chat.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const socketio = require('socket.io');
+
+const PORT = 9001
+
 const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-const expressServer = app.listen(9001)
+const expressServer = app.listen(PORT)
+
+// Call the SocketIO server constructor to expose the server.
 const io = socketio(expressServer)
+console.log("Listening to port " + PORT)
+
 io.on('connection', (socket) => {
   socket.emit('messageFromServer', {data: "Welcome to the socketio server"})
   socket.on('messageToServer', (dataFromClient) => {
